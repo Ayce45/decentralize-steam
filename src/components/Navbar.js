7f03d40a96db9d66a9ce58d2dc43a02838dda2c0
Jdevvv <llanta.jeremy@gmail.com>
2022-04-20 13:40:37 +0200

    🔥 remove unused item nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -130,32 +130,6 @@ class Navbar extends Component {
 														</Link>
 													)}
 												</Menu.Item>
-												<Menu.Item>
-													{({ active }) => (
-														<a
-															href="google.com"
-															className={classNames(
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm text-gray-700'
-															)}
-														>
-															Settings
-														</a>
-													)}
-												</Menu.Item>
-												<Menu.Item>
-													{({ active }) => (
-														<a
-															href="google.com"
-															className={classNames(
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm text-gray-700'
-															)}
-														>
-															Sign out
-														</a>
-													)}
-												</Menu.Item>
 											</Menu.Items>
 										</Transition>
 									</Menu>
